c0e08313d16fe213cacb72c9caea4d64ed3634b2
Gordon Gustafson <gordon3.14@gmail.com>
2017-01-15 11:28:14 -0500

    Update to Clojure 1.9 alpha for core.spec

--- project.clj
+++ project.clj
@@ -3,8 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/core.match "0.3.0-alpha4"]]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+                 [org.clojure/core.match "0.3.0-alpha4"]
+                 ;; TODO: make this a :dev or :test dependency
+                 [org.clojure/test.check "0.9.0"]]
   :main ^:skip-aot diplomacy.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
