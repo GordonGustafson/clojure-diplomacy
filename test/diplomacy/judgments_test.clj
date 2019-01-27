59eca973353d8280432f17e4f42349d5dbf89e4d
Gordon Gustafson <gordon3.14@gmail.com>
2019-01-27 12:08:20 -0800

    Add another test to judgments-test
    I wasn't even asserting anything before since I used `is`
    incorrectly *facepalm*.

--- test/diplomacy/judgments_test.clj
+++ test/diplomacy/judgments_test.clj
@@ -7,12 +7,24 @@
    :destination :lon})
 
 (deftest test-create-attack-judgment
-  (is (create-attack-judgment
-       :interfered? true
-       :interferer french-army-bre-to-lon
-       :attack-rule :attacked-same-destination))
-  {:interfered? true
-   :interferer french-army-bre-to-lon
-   :conflict-situation {:attack-conflict-rule :attacked-same-destination
-                        :beleaguered-garrison-changing-outcome nil}
-   :would-dislodge-own-unit? false})
+  (is (= (create-attack-judgment
+          :interfered? true
+          :interferer french-army-bre-to-lon
+          :attack-rule :attacked-same-destination)
+         {:interfered? true
+          :interferer french-army-bre-to-lon
+          :conflict-situation {:attack-conflict-rule :attacked-same-destination
+                               :beleaguered-garrison-changing-outcome nil}
+          :would-dislodge-own-unit? false}))
+
+  ;; This order is nonsense, but I just want a quick test
+  (is (= (create-attack-judgment
+          :interfered? true
+          :interferer french-army-bre-to-lon
+          :attack-rule :attacked-same-destination
+          :would-dislodge-own-unit? true)
+         {:interfered? true
+          :interferer french-army-bre-to-lon
+          :conflict-situation {:attack-conflict-rule :attacked-same-destination
+                               :beleaguered-garrison-changing-outcome nil}
+          :would-dislodge-own-unit? true})))
